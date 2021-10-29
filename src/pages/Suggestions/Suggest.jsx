cc5560cc0ec94861b838ef776d8423c9998ecdb7
Muhammadrizo <yusufovmuhammadrizodev@gmail.com>
2021-10-29 11:54:38 +0500

    suggestions page done

--- src/pages/Suggestions/Suggest.jsx
+++ src/pages/Suggestions/Suggest.jsx
@@ -1,8 +1,39 @@
-const Suggest = () => {
+import './Suggestions.css'
 
+const Suggest = () => {
     return (
-      <div>
-        <h1>Takliflar</h1>
+      <div className="suggestions-box">
+        <h2 className="suggestions-heading">Taklif va shikoyatlar</h2>
+        <ul className="suggestions-list">
+          <li className="suggestions-list-item">
+            <div className="suggenstions-list-item-texts">
+              <div className="circle">
+                22
+              </div>
+              <h5 className="suggestions-list-item-heading">Bobur Ganiyev</h5>
+              <p>01/01/2021</p>
+            </div>
+            <p>Imperdiet arcu auctor ac quis at adipiscing odio morbi blandit. Purus ornare mattis tincidunt tellus mauris malesuada facilisi urna. Libero, feugiat volutpat diam nullam duis dui sed pulvinar. Nunc duis facilisis fringilla etiam. Lorem dignissim molestie volutpat in dolor arcu. In vitae fusce dignissim sodales.</p>
+            <div className="suggestions-list-item-buttons">
+              <button className="share">null</button>
+              <button className="delete">null</button>
+            </div>
+          </li>
+          <li className="suggestions-list-item">
+            <div className="suggenstions-list-item-texts">
+              <div className="circle">
+                22
+              </div>
+              <h5 className="suggestions-list-item-heading">Bobur Ganiyev</h5>
+              <p>01/01/2021</p>
+            </div>
+            <p>Imperdiet arcu auctor ac quis at adipiscing odio morbi blandit. Purus ornare mattis tincidunt tellus mauris malesuada facilisi urna. Libero, feugiat volutpat diam nullam duis dui sed pulvinar. Nunc duis facilisis fringilla etiam. Lorem dignissim molestie volutpat in dolor arcu. In vitae fusce dignissim sodales.</p>
+            <div className="suggestions-list-item-buttons">
+              <button className="share">null</button>
+              <button className="delete">null</button>
+            </div>
+          </li>
+        </ul>
       </div>
     )
   }
