f307d1c73887ba21eedc0ddbe202b28767fa626e
Muhammadrizo <yusufovmuhammadrizodev@gmail.com>
2021-11-01 22:18:32 +0500

    settings page improvements

--- src/pages/Settings/settings.jsx
+++ src/pages/Settings/settings.jsx
@@ -9,6 +9,31 @@ const Settings = () => {
         <div className="item"><a href="http://localhost:3000/settings/infos">Infos</a></div>
         <div className="item"><a href="http://localhost:3000/settings/payment">Payment</a></div>
       </div>
+
+      <h2 className="available-users">Available users</h2>
+      <ul className="users-list">
+        <li className="users-list-item">
+          <p>1. John Doe</p>
+          <div className="buttons">
+            <button className="edit-button"></button>
+            <button className="delete-button"></button>
+          </div>
+        </li>
+        <li className="users-list-item">
+          <p>2. Shuhrat</p>
+          <div className="buttons">
+            <button className="edit-button"></button>
+            <button className="delete-button"></button>
+          </div>
+        </li>
+        <li className="users-list-item">
+          <p>3. Ilhom</p>
+          <div className="buttons">
+            <button className="edit-button"></button>
+            <button className="delete-button"></button>
+          </div>
+        </li>
+      </ul>
     </>
   )
 }
