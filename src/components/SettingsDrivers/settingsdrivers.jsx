08571e3c4caf786b96f54fe3454b2be4240c48ce
Muhammadrizo <yusufovmuhammadrizodev@gmail.com>
2021-11-03 10:55:15 +0500

    settings/drivers completed

---
+++ src/components/SettingsDrivers/settingsdrivers.jsx
@@ -0,0 +1,72 @@
+import './settingsdrivers.css'
+
+function SettingsDrivers() {
+    return (
+        <>
+            <table className="orders-table">
+                <tr className="orders-table__header">
+                    <th className="orders-table__id table_id-width rows-hover">ID</th>
+                    <th className="orders-table__fullname table_fullname-width">Familiya ismi</th>
+                    <th className="orders-table__dob table_dob-width">D.O.B</th>
+                    <th className="orders-table__phone table_phone-width ">Telefon raqam</th>
+                    <th className="orders-autonumber__price table_autonumber-width ">Avtoraqam</th>
+                    <th className="orders-table__type table_type-width ">Turi</th>
+                    <th className="orders-table__status table_status-width ">Holat</th>
+                    <th className="orders-table__edit table_edit-width ">Tahrirlash</th>
+                </tr>
+                <tr className="orders-table__body">
+                    <td className="orders-table__id-content table_id-width ">6</td>
+                    <td className="orders-table__name-content table_name-width "><a href="javascript:void(0)" className="order-name__link">Qudratullayev Omonjon</a></td>
+                    <td className="orders-table_dob-content table_name-width">22.03.1995</td>
+                    <td className="orders-table__phone-content table_phone-width "> <a href="javascript:void(0)" className="order-phone__link">+998901234567</a></td>
+                    <td className="orders-table__price-content table_price-width ">20 A 000 CC</td>
+                    <td className="orders-table__type-content table_type-width ">Damas</td>
+                    <td className="orders-table__driver-content table_driver-width ">NOT SELECTED</td>
+                    <td className="orders-table__more-content table_more-width "><button>...</button></td>
+                </tr>
+                <tr className="orders-table__body">
+                    <td className="orders-table__id-content table_id-width ">6</td>
+                    <td className="orders-table__name-content table_name-width "><a href="javascript:void(0)" className="order-name__link">Qudratullayev Omonjon</a></td>
+                    <td className="orders-table_dob-content table_name-width">22.03.1995</td>
+                    <td className="orders-table__phone-content table_phone-width "> <a href="javascript:void(0)" className="order-phone__link">+998901234567</a></td>
+                    <td className="orders-table__price-content table_price-width ">20 A 000 CC</td>
+                    <td className="orders-table__type-content table_type-width ">Nexia 3</td>
+                    <td className="orders-table__driver-content table_driver-width ">NOT SELECTED</td>
+                    <td className="orders-table__more-content table_more-width "><button>...</button></td>
+                </tr>
+                <tr className="orders-table__body">
+                    <td className="orders-table__id-content table_id-width ">6</td>
+                    <td className="orders-table__name-content table_name-width "><a href="javascript:void(0)" className="order-name__link">Qudratullayev Omonjon</a></td>
+                    <td className="orders-table_dob-content table_name-width">22.03.1995</td>
+                    <td className="orders-table__phone-content table_phone-width "> <a href="javascript:void(0)" className="order-phone__link">+998901234567</a></td>
+                    <td className="orders-table__price-content table_price-width ">20 A 000 CC</td>
+                    <td className="orders-table__type-content table_type-width ">Matiz</td>
+                    <td className="orders-table__driver-content table_driver-width ">NOT SELECTED</td>
+                    <td className="orders-table__more-content table_more-width "><button>...</button></td>
+                </tr>
+                <tr className="orders-table__body">
+                    <td className="orders-table__id-content table_id-width ">6</td>
+                    <td className="orders-table__name-content table_name-width "><a href="javascript:void(0)" className="order-name__link">Qudratullayev Omonjon</a></td>
+                    <td className="orders-table_dob-content table_name-width">22.03.1995</td>
+                    <td className="orders-table__phone-content table_phone-width "> <a href="javascript:void(0)" className="order-phone__link">+998901234567</a></td>
+                    <td className="orders-table__price-content table_price-width ">20 A 000 CC</td>
+                    <td className="orders-table__type-content table_type-width ">Spark</td>
+                    <td className="orders-table__driver-content table_driver-width ">NOT SELECTED</td>
+                    <td className="orders-table__more-content table_more-width "><button>...</button></td>
+                </tr>
+                <tr className="orders-table__body">
+                    <td className="orders-table__id-content table_id-width ">6</td>
+                    <td className="orders-table__name-content table_name-width "><a href="javascript:void(0)" className="order-name__link">Qudratullayev Omonjon</a></td>
+                    <td className="orders-table_dob-content table_name-width">22.03.1995</td>
+                    <td className="orders-table__phone-content table_phone-width "> <a href="javascript:void(0)" className="order-phone__link">+998901234567</a></td>
+                    <td className="orders-table__price-content table_price-width ">20 A 000 CC</td>
+                    <td className="orders-table__type-content table_type-width ">Malibu</td>
+                    <td className="orders-table__driver-content table_driver-width ">NOT SELECTED</td>
+                    <td className="orders-table__more-content table_more-width "><button>...</button></td>
+                </tr>
+            </table>
+        </>
+    )
+}
+
+export default SettingsDrivers
--- src/pages/Settings/settings.jsx
+++ src/pages/Settings/settings.jsx
@@ -1,12 +1,13 @@
 import SettingsNestedLink from '../../components/SettingsNestedLink/settingsnestedlink.jsx'
 import SettingsUsersList from '../../components/SettingsUsersList/settingsuserslist.jsx'
 import SettingsChange from '../../components/SettingsChange/settingschange.jsx'
+import SettingsDrivers from '../../components/SettingsDrivers/settingsdrivers.jsx'
 
 function Settings () {
   return (
     <>  
       <SettingsNestedLink/>
-      <SettingsChange/>
+      <SettingsDrivers/>
     </>
   )
 }
