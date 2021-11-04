37714a808b4ce6eca75a4d68a1112c96450013eb
FastDelivery1 <doni404error@gmail.com>
2021-11-04 17:12:02 +0500

    modal finished

--- src/components/AuthorizationBox/auth.jsx
+++ src/components/AuthorizationBox/auth.jsx
@@ -1,18 +1,24 @@
 import './auth.css'
 
 function AuthBox () {
+
+	
+
     return (
         <div className="authorization-box">
 				<h2 className="authorization-heading">Kirish</h2>
-				<form className="authorization-form">
+				<form  className="authorization-form" method="POST" action="https://fastdelevery.herokuapp.com/login">
 					<div className="login-input-wrap">
-						<input className="authorization-login-input" placeholder="Login" type="text"/>
+						<input className="authorization-login-input" placeholder="Login" type="text" name='name'/>
 					</div>
 					<div className="password-input-wrap">
-						<input className="authorization-password-input" placeholder="Parol" type ="password"/>
+						<input className="authorization-password-input" placeholder="Parol" type ="password" name='password'/>
 					</div>
 					<button className="authorization-button" type ="submit">Kirish</button>
 				</form>
+				<script type="text/javascript">
+					console.log(document.cookie)
+				</script>
 		</div>
     )
 }
