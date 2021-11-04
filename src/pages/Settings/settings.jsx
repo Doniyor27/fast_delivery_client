0f34a34522704c111893d3a17456e28b97bdb7b7
Muhammadrizo <yusufovmuhammadrizodev@gmail.com>
2021-11-04 11:06:01 +0500

    edit category modal added

--- src/pages/Settings/settings.jsx
+++ src/pages/Settings/settings.jsx
@@ -4,12 +4,13 @@ import SettingsChange from '../../components/SettingsChange/settingschange.jsx'
 import SettingsDrivers from '../../components/SettingsDrivers/settingsdrivers.jsx'
 import SettingsPayment from '../../components/SettingsPayment/settingspayment.jsx'
 import AddCategoryModal from '../../components/AddCategoryModal/addcategorymodal.jsx'
+import EditCategoryModal from '../../components/EditCategoryModal/editcategorymodal.jsx'
 
 function Settings () {
   return (
     <>  
       <SettingsNestedLink/>
-      <AddCategoryModal/>
+      <EditCategoryModal/>
     </>
   )
 }
