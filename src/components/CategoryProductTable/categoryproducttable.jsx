e2213545534a69650dfb0c14041680a6e98c8d47
Muhammadrizo <yusufovmuhammadrizodev@gmail.com>
2021-11-16 00:01:58 +0500

    tables' width fixed

--- src/components/CategoryProductTable/categoryproducttable.jsx
+++ src/components/CategoryProductTable/categoryproducttable.jsx
@@ -6,19 +6,15 @@ function CategoryProductTable () {
             <table className="category-product-table">
                 <tr className="category-product-table__header">
                     <th className="category-product-table__id category-product-table_id-width rows-hover">ID</th>
-                    <th className="category-product-table__stores category-product-table_stores-width">do'konlar</th>
+                    <th className="category-product-table__product category-product-table_product-width">tovar</th>
                     <th className="category-product-table__count category-product-table_count_width">tovar soni</th>
-                    <th className="category-product-table__phone category-product-table_phone_width">telefon raqam</th>
-                    <th className="category-product-table__location category-product-table_location_width">manzil</th>
                     <th className="category-product-table__status category-product-table_status-width">holat</th>
                     <th className="category-product-table__edit category-product-table_edit_width">tahrirlash</th>
                 </tr>
                 <tr className="category-product-table__body">
                     <td className="category-product-table__id category-product-table_id-width rows-hover">52</td>
-                    <td className="category-product-table__stores category-product-table_stores-width">Evos</td>
+                    <td className="category-product-table__product category-product-table_product-width">lavash</td>
                     <td className="category-product-table__count category-product-table_count_width">5</td>
-                    <td className="category-product-table__phone category-product-table_phone_width">+998901234567</td>
-                    <td className="category-product-table__location category-product-table_location_width">Qo'shtepa</td>
                     <td className="category-product-table__status category-product-table_status-width">null</td>
                     <td className="category-product-table__edit category-product-table_edit_width">null</td>
                 </tr>
